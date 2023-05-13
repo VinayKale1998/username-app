473a1099b9b36461243538fa97e594dadfec3db7
VinaKale1998 <vinaykale1999@gmail.com>
2023-05-13 09:24:15 +0530

    header

--- food-app/src/App.js
+++ food-app/src/App.js
@@ -1,11 +1,11 @@
-
-
-const App=()=>{
-
-return(
-  null
-)
-}
+import Header from "./Components/Layout/Header";
+import React from "react";
+const App = () => {
+  return (
+    <React.Fragment>
+      <Header></Header>
+    </React.Fragment>
+  );
+};
 
 export default App;
-
---
+++ food-app/src/Components/Layout/HeaderButton.js
@@ -0,0 +1,20 @@
+import CartIcon from "../Cart/CartIcon";
+import classes from  './HeaderCartButton.module.css'
+
+const HeaderButton = (props) => {
+  return (
+
+    <button className={classes.button}>
+      <span className={classes.icon}>
+        <CartIcon></CartIcon>{" "}
+      </span>
+
+      <span >Cart</span>
+      <span className={classes.badge}>3</span>
+
+      
+    </button>
+  );
+};
+
+export default HeaderButton;
