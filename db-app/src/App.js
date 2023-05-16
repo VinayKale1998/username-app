3b8c22df99089ddcc75e366cfa786e1744ad155c
VinaKale1998 <vinaykale1999@gmail.com>
2023-05-16 14:14:47 +0530

    db

--- db-app/src/App.js
+++ db-app/src/App.js
@@ -1,38 +1,51 @@
-import React, {useState}from "react";
+import React, { useState } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 
-
 function App() {
-
-
   const [data, setData] = useState([]);
+  const [isloading, setLoading] = useState(false);
+  const [errorMessage, setError] = useState(null);
 
-  const fetchData = () => {
-    fetch("https://swapi.py4e.com/api/films")
-      .then((response) => {  
-        return response.json();
-      })
-      .then((data) => {
-        const movies = data.results.map((movie) => {
-          return {
-            title: movie.title,
-            openingText: movie.opening_crawl,
-            releaseDate: movie.release_date,
-          };
-        });
-        setData(movies);
+  const fetchData = async () => {
+    setLoading(true);
+    setError(null);
+    try {
+      const response = await fetch("https://swapi.py4e.com/api/film");
+      if (!response.ok) {
+        const error = new Error("something went wrong");
+        throw error;
+      }
+      
+      
+      const data = await response.json();
+      
+
+      const movies = data.results.map((movie) => {
+        return {
+          title: movie.title,
+          openingText: movie.opening_crawl,
+          releaseDate: movie.release_date,
+        };
       });
+      setData(movies);
+    } catch (error) {
+      setError(error.message);
+    }
+    setLoading(false);
   };
 
   return (
     <React.Fragment>
       <section>
-        <button onClick={fetchData} >Fetch Movies</button>
+        <button onClick={fetchData}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={data} />
+        {isloading && data.length === 0 && <h1>Loading.....</h1>}
+        {!isloading && data.length > 0 && <MoviesList movies={data} />}
+        {!isloading && data.length == 0 && !errorMessage && <h1> No movies found</h1>}
+        {!isloading && errorMessage != null && <h1>{errorMessage}</h1>}
       </section>
     </React.Fragment>
   );
