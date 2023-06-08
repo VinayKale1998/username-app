2ecce46eb31a10ebda4f8a6605ca3381658dd3b8
VinaKale1998 <vinaykale1999@gmail.com>
2023-06-08 11:38:44 +0530

    Cleaning project

--- card-app/src/Pages/CreateFlashCard.js
+++ card-app/src/Pages/CreateFlashCard.js
@@ -1,4 +1,4 @@
-import { Outlet,NavLink } from "react-router-dom";
+import { Outlet, NavLink } from "react-router-dom";
 import MainNavigation from "../Components/MainNavigation";
 
 import Demo from "../Components/Demo";
@@ -19,46 +19,46 @@ const CreateFlashCard = () => {
             FlashKing
           </span>
         </NavLink>
-        {/* <nav>
-          <ul className="flex space-x-16 mr-20 ">
-            <li className="cursor-pointer border border-purple-300 px-4  py-4 w-20 bg- place-self-end rounded-md hover:bg-white  hover:text-purple-900 text-white">
-              Home
-            </li>
-            <li className="cursor-pointer border px-4 w-20 bg py-4  place-self-end rounded-md hover:bg-white hover:text-purple-900 text-white">
-              About{" "}
-            </li>
-            <li className="cursor-pointer border px-2 w-20 bg py-4  place-self-end rounded-md hover:bg-white  hover:text-purple-900 text-white">
-              Contact
-            </li>
-          </ul>
-        </nav> */}
       </div>
       <div className="body  bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400 flex w-full flex-row">
-    {/* section */}
-    <section className="main mt-1 mx-36 w-9/12 flex flex-col">
-      {/* heading */}
-      <div className="heading ml-[32%] ">
-        <h1 className="t font-bold text-4xl text-indigo-900">Create Flash Card</h1>
-      </div>
-      {/* Nav buttons */}
-      <div className="buttons flex space-x-0 w-100  mt-0 mb-0">
-        <NavLink to='/'  className={ ({isActive})=>isActive?` text-white border-purple-900 border-2  mr-0 outline-none border-t-0 border-l-0 border-r-0 border-b-violet-800-700 pb-3 font-bold text-2xl  `: `  border-purple-900  outline-none border-t-0 border-l-0 border-r-0 border-b-violet-800-700 pb-3 font-bold text-2xl `}>
-        <span className="mx-10"> CreateNew</span>
-        </NavLink>
+        {/* section */}
+        <section className="main mt-1 mx-36 w-9/12 flex flex-col">
+          {/* heading */}
+          <div className="heading ml-[32%] ">
+            <h1 className="t font-bold text-4xl text-indigo-900">
+              Create Flash Card
+            </h1>
+          </div>
+          {/* Nav buttons */}
+          <div className="buttons flex space-x-0 w-100  mt-0 mb-0">
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive
+                  ? ` text-white border-purple-900 border-2  mr-0 outline-none border-t-0 border-l-0 border-r-0 border-b-violet-800-700 pb-3 font-bold text-2xl  `
+                  : `  border-purple-900  outline-none border-t-0 border-l-0 border-r-0 border-b-violet-800-700 pb-3 font-bold text-2xl `
+              }
+            >
+              <span className="mx-10"> CreateNew</span>
+            </NavLink>
 
-        <NavLink  to='/MyFlashCards'className={ ({isActive})=>isActive?` text-white border-purple-900   mr-0 border-2 outline-none border-t-0 border-l-0 border-r-0 border-b-violet-800-700 pb-3 font-bold text-2xl  `: `  border-purple-900  outline-none border-t-0 border-l-0 border-r-0 border-b-violet-800-700 pb-3 font-bold text-2xl `}>
-           <span className="mx-10">My Flashcards</span>
-        </NavLink>
+            <NavLink
+              to="/MyFlashCards"
+              className={({ isActive }) =>
+                isActive
+                  ? ` text-white border-purple-900   mr-0 border-2 outline-none border-t-0 border-l-0 border-r-0 border-b-violet-800-700 pb-3 font-bold text-2xl  `
+                  : `  border-purple-900  outline-none border-t-0 border-l-0 border-r-0 border-b-violet-800-700 pb-3 font-bold text-2xl `
+              }
+            >
+              <span className="mx-10">My Flashcards</span>
+            </NavLink>
+          </div>
+          {/* First Form */}
+          {/* <Group_Form></Group_Form> */}
+          <Outlet></Outlet>
+        </section>
       </div>
-      {/* First Form */}
-    {/* <Group_Form></Group_Form> */}
-    <Outlet></Outlet>
-
-    </section>
-  </div>
-     
     </div>
-   
   );
 };
 
